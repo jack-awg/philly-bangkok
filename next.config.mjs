2cc734f7d961610a0899ec9d216e3ef8add1c619
jack-awg <jack.seddon@audleywestgroup.com>
2026-02-05 15:47:00 +0700

    ESLint disabled during builds

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ const nextConfig = {
   productionBrowserSourceMaps: true,
   distDir: process.env.DIST_DIR || '.next',
 
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
   images: {
     remotePatterns: [
       {
